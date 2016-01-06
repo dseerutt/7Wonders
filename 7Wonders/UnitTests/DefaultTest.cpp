6c55e760ec16b952c7c730e9cbc319a57231c501
Dan <dan.seeruttun@gmail.com>
2016-01-06 13:10:04 +0100

    unit testing

--- 7Wonders/UnitTests/DefaultTest.cpp
+++ 7Wonders/UnitTests/DefaultTest.cpp
@@ -17,5 +17,20 @@ namespace UnitTests
 			BlueCard c("ee", 5);
 		}
 
+		TEST_METHOD(DefaultTestsMethod)
+		{
+			// TODO: Ici, votre code de test
+			Assert::AreEqual(0, 0);
+			Assert::IsFalse(10 < 2);
+			Assert::IsTrue(0 < 2);
+			auto func = [this] { throw(1); };
+
+			Assert::ExpectException<int>(func);
+
+
+			auto func2 = [this] { throw("rr"); };
+
+			Assert::ExpectException<char*>(func2); //Pas string
+		}
 	};
 }
