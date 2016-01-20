60b0ecfcace50c240e89c10c565a1bfa727def01
Dan <dan.seeruttun@gmail.com>
2016-01-20 09:55:45 +0100

    vcx proj

--- 7Wonders/UnitTests/TestingScoreTest.cpp
+++ 7Wonders/UnitTests/TestingScoreTest.cpp
@@ -16,10 +16,137 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestingScore1)
+		TEST_METHOD(TestingWar)
 		{
-			World w(3, 2);
-			Assert::IsFalse(w.m_gameOver);
+			World w(0, 3);
+			while (!w.m_gameOver)
+			{
+				if (w.betweenTurns())
+				{
+					int v0 = w.m_scores.at(0).at(WAR);
+					int v1 = w.m_scores.at(1).at(WAR);
+					int v2 = w.m_scores.at(2).at(WAR);
+					w.updateMilitary();
+					int factor = 0;
+					if (w.m_age == 1)
+					{
+						factor = 1;
+					}
+					if (w.m_age == 2)
+					{
+						factor = 3;
+					}
+					if (w.m_age == 3)
+					{
+						factor = 5;
+					}
+					int newValue;
+					//J 1
+					newValue = 0;
+					if (w.m_players.at(0)->getMilitary() > w.m_players.at(0)->leftNeighbor->getMilitary())
+					{
+						newValue += factor;
+					}
+					if (w.m_players.at(0)->getMilitary() < w.m_players.at(0)->leftNeighbor->getMilitary())
+					{
+						newValue += -1;
+					}
+					if (w.m_players.at(0)->getMilitary() > w.m_players.at(0)->rightNeighbor->getMilitary())
+					{
+						newValue += factor;
+					}
+					if (w.m_players.at(0)->getMilitary() < w.m_players.at(0)->rightNeighbor->getMilitary())
+					{
+						newValue += -1;
+					}
+					Assert::AreEqual(newValue + v0, w.m_scores.at(0).at(WAR));
+
+					//J2
+					newValue = 0;
+					if (w.m_players.at(1)->getMilitary() > w.m_players.at(1)->leftNeighbor->getMilitary())
+					{
+						newValue += factor;
+					}
+					if (w.m_players.at(1)->getMilitary() < w.m_players.at(1)->leftNeighbor->getMilitary())
+					{
+						newValue += -1;
+					}
+					if (w.m_players.at(1)->getMilitary() > w.m_players.at(1)->rightNeighbor->getMilitary())
+					{
+						newValue += factor;
+					}
+					if (w.m_players.at(1)->getMilitary() < w.m_players.at(1)->rightNeighbor->getMilitary())
+					{
+						newValue += -1;
+					}
+					Assert::AreEqual(newValue + v1, w.m_scores.at(1).at(WAR));
+
+					//J3
+					 newValue = 0;
+					if (w.m_players.at(2)->getMilitary() > w.m_players.at(2)->leftNeighbor->getMilitary())
+					{
+						newValue += factor;
+					}
+					if (w.m_players.at(2)->getMilitary() < w.m_players.at(2)->leftNeighbor->getMilitary())
+					{
+						newValue += -1;
+					}
+					if (w.m_players.at(2)->getMilitary() > w.m_players.at(2)->rightNeighbor->getMilitary())
+					{
+						newValue += factor;
+					}
+					if (w.m_players.at(2)->getMilitary() < w.m_players.at(2)->rightNeighbor->getMilitary())
+					{
+						newValue += -1;
+					}
+					Assert::AreEqual(newValue + v2, w.m_scores.at(2).at(WAR));
+
+					w.startAge();
+				}
+				Player& p = *w.m_players[0];
+				w.play(p);
+				w.playOthers(p);
+				w.endTurn();
+			}
+			Assert::IsTrue(w.m_gameOver);
+		}
+
+		TEST_METHOD(TestingMarvelPoints)
+		{
+			World w(0, 5);
+			w.run();
+			Assert::IsTrue(w.m_gameOver);
+			for (int i = 0; i < w.m_players.size(); i++)
+			{
+				Assert::AreEqual(w.m_players.at(i)->getMarvel()->getMarvelScore(),w.m_scores.at(i).at(WONDER_LEVEL));
+			}
+		}
+
+		TEST_METHOD(GoldPoints)
+		{
+			World w(0, 5);
+			w.run();
+			Assert::IsTrue(w.m_gameOver);
+			for (int i = 0; i < w.m_players.size(); i++)
+			{
+				Assert::AreEqual(w.m_players.at(i)->getMoney() / 3, (unsigned int) w.m_scores.at(i).at(GOLD));
+			}
+		}
+
+		TEST_METHOD(BluePoints)
+		{
+			World w(0, 5);
+			w.run();
+			Assert::IsTrue(w.m_gameOver);
+			for (int i = 0; i < w.m_players.size(); i++)
+			{
+				int bluePoints = 0;
+				for (int j = 0; j < w.m_players.at(i)->getBoard().size(); j++)
+				{
+
+				}
+				Assert::AreEqual(w.m_players.at(i)->getMoney() / 3, (unsigned int)w.m_scores.at(i).at(GOLD));
+			}
 		}
 
 		//TOTEST
@@ -34,11 +161,7 @@ namespace UnitTests
 		3 GUILDE_DES_SCIENTIFIQUES
 		3 GUILDE_DES_MAGISTRATS
 		3 GUILDE_DES_BATISSEURS
-		Points de merveille
 		Points jaunes
-		WAR,
-		GOLD,
-		WONDER_LEVEL,
 		BLUE_CARDS,
 		YELLOW_CARDS,
 		GUILDS,
